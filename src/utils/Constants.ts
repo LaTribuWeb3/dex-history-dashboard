df1d8baad9e3494163226b866a89f0a643b10178
Alfu <Alfulinku@gmail.com>
2024-03-11 15:34:29 +0100

    add balancer

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,11 +1,12 @@
-export const DATA_SOURCES = ['All sources', 'Uniswap v2', 'Uniswap v3', 'Curve', 'Sushiswap v2'];
+export const DATA_SOURCES = ['All sources', 'Uniswap v2', 'Uniswap v3', 'Curve', 'Sushiswap v2', 'Balancer'];
 
 export const DATA_SOURCES_MAP = {
   'All sources': 'all',
   'Uniswap v2': 'uniswapv2',
   'Uniswap v3': 'uniswapv3',
   Curve: 'curve',
-  'Sushiswap v2': 'sushiswapv2'
+  'Sushiswap v2': 'sushiswapv2',
+  'Balancer': 'balancer'
 };
 
 export const SLIPPAGES_BPS = Array.from({ length: 20 }).map((_v, i) => (i + 1) * 100);
